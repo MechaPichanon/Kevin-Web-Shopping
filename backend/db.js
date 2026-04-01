12b418424774485f8869db4c70009162d31359f9
delafaye1111 <delafayesql@gmail.com>
2026-04-01 17:19:42 +0700

    Attempt4Integrate(4/1/2026)

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,7 @@
 const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "bos_butter",
-  password: "bosbutter@db",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = pool;
